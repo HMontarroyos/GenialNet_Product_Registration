ee42c7a464842fdd5030b2887d10b9ea1d86a1d4
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2024-02-22 18:26:38 -0300

    feat: create view Home

---
+++ src/views/Home/index.tsx
@@ -0,0 +1,11 @@
+import { Button } from "../../components";
+
+function App() {
+  return (
+    <>
+    <Button onClick={() => console.log("TESTE")} text={"Cadastrar Produtos"} />
+    </>
+  );
+}
+
+export default App;
